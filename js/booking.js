6b56657e65404e7f7e2cedc2a9131e794f2304ec
Daniel Sørlie Hoftvedt <danisho@stud.ntnu.no>
2021-11-09 11:02:08 +0100

    la til booking.js i main.js

--- js/booking.js
+++ js/booking.js
@@ -1,35 +1,7 @@
-
-const vaskeri =document.getElementById("vaskeri")
-const bakgård =document.getElementById("bakgård")
-const body =document.querySelector("body")
-var linebreak = document.createElement("br")
-
-
-
-
-
-const bookingTimes=["06:00","07:00", "08:00", "09:00",
-             "10:00", "11:00", "12:00", "13:00", "14:00",
-             "15:00", "16:00", "17:00", "18:00", "19:00", 
-             "20:00", "21:00", "22:00", "23:00"]
-
-const vaskeriList = document.createElement("ul")
-vaskeriList.innerText="Vaskeri"
-vaskeriList.appendChild(linebreak)
-
-const bakgårdList= document.createElement("ul")
-bakgårdList.innerText="Bakgård"
-bakgårdList.appendChild(linebreak)
-
-console.log("nicenice")
-
-     
-
 function makeButtonsforVaskeri(){
     for (let i=0; i<bookingTimes.length; i++){
-        const button= document.createElement("button")
-        const input= document.createElement("input")
-        var linebreak = document.createElement("br");
+        const button = document.createElement("button")
+        const input = document.createElement("input")
         button.innerText= bookingTimes[i];
         button.onclick =function bookTime(){
             if (button.style.backgroundColor=="red"){
@@ -38,10 +10,8 @@ function makeButtonsforVaskeri(){
             else {
                 button.style.backgroundColor="red"
             }
-            
-            console.log("nice");
         }
-        vaskeriList.appendChild(linebreak)
+        vaskeriList.appendChild(document.createElement("br"))
         vaskeriList.appendChild(button)
         vaskeriList.appendChild(input)
         
@@ -52,34 +22,35 @@ function makeButtonsforBakgård(){
     for (let i=0; i<bookingTimes.length; i++){
         const button= document.createElement("button")
         const input= document.createElement("input")
-        var linebreak = document.createElement("br");
-        button.innerText= bookingTimes[i];
-        button.onclick =function bookTime(){
+        button.innerText = bookingTimes[i];
+        button.onclick = function bookTime(){
             if (button.style.backgroundColor=="red"){
                 button.style.backgroundColor="green"
             }
             else {
                 button.style.backgroundColor="red"
             }
-            
-            console.log("nice");
         }
-        bakgårdList.appendChild(linebreak)
+        bakgårdList.appendChild(document.createElement("br"))
         bakgårdList.appendChild(button)
         bakgårdList.appendChild(input)
         
     }
 }
 
+const bookingTimes=["06:00","07:00", "08:00", "09:00",
+             "10:00", "11:00", "12:00", "13:00", "14:00",
+             "15:00", "16:00", "17:00", "18:00", "19:00", 
+             "20:00", "21:00", "22:00", "23:00"]
+
+const vaskeriList = document.getElementById("vaskeriList")
+vaskeriList.innerText = "Vaskeri"
+
+const bakgårdList = document.getElementById("bakgårdList")
+bakgårdList.innerText = "Bakgård"
 
 makeButtonsforVaskeri();
 makeButtonsforBakgård();
-
-
-console.log("nice")
-
-
-              
 
 // lage en array med klokketidet
 // append child button og button text og id
--- js/main.js
+++ js/main.js
@@ -1,82 +1,41 @@
-function booking(){
-    const vaskeri =document.getElementById("vaskeri")
-    const bakgård =document.getElementById("bakgård")
-    //var button = document.querySelector("button")
-    const body =document.querySelector("body")
-    
-
-    //buttons.forEach(addEventListener("click", bookTime))
-    
-
-    const bookingTimes=["06:00","07:00", "08:00", "09:00",
-                 "10:00", "11:00", "12:00", "13:00", "14:00",
-                 "15:00", "16:00", "17:00", "18:00", "19:00", 
-                 "20:00", "21:00", "22:00", "23:00"]
-    
-    const vaskeriList = document.createElement("ul")
-    vaskeriList.innerText="Vaskeri"
-    
-    const bakgårdList= document.createElement("ul")
-    bakgårdList.innerText="Bakgård"
-    
-    console.log("nicenice")
-    
-
-    function makeButtonsforVaskeri(){
-        for (let i=0; i<bookingTimes.length; i++){
-            const button= document.createElement("button")
-            const input= document.createElement("input")
-            button.innerText= bookingTimes[i];
-            button.onclick =function bookTime(){
-                if (button.style.backgroundColor=="red"){
-                    button.style.backgroundColor="green"
-                }
-                else {
-                    button.style.backgroundColor="red"
-                }
-                
-                console.log("nice");
+function makeButtonsforVaskeri(bookingTimes){
+    for (let i=0; i<bookingTimes.length; i++){
+        const button = document.createElement("button")
+        const input = document.createElement("input")
+        button.innerText= bookingTimes[i];
+        button.onclick =function bookTime(){
+            if (button.style.backgroundColor=="red"){
+                button.style.backgroundColor="green"
             }
-            
-            vaskeriList.appendChild(button)
-            vaskeriList.appendChild(input)
-            //button.addEventListener("click", bookTime)
-        }
-    }
-    
-    function makeButtonsforBakgård(){
-        for (let i=0; i<bookingTimes.length; i++){
-            const button= document.createElement("button")
-            const input= document.createElement("input")
-            button.innerText= bookingTimes[i];
-            button.onclick =function bookTime(){
-                if (button.style.backgroundColor=="red"){
-                    button.style.backgroundColor="green"
-                }
-                else {
-                    button.style.backgroundColor="red"
-                }
-                
-                console.log("nice");
+            else {
+                button.style.backgroundColor="red"
             }
-            
-            bakgårdList.appendChild(button)
-            bakgårdList.appendChild(input)
-            //button.addEventListener("click", bookTime)
         }
+        vaskeriList.appendChild(document.createElement("br"))
+        vaskeriList.appendChild(button)
+        vaskeriList.appendChild(input)
+        
     }
-    
-    makeButtonsforVaskeri();
-    
-    body.appendChild(vaskeriList)
-    makeButtonsforBakgård();
-    
-    body.appendChild(bakgårdList)
-    
-    console.log("nice")
-    // lage en array med klokketidet
-    // append child button og button text og id
-    //lage elementer dånn x
+}
+
+function makeButtonsforBakgård(bookingTimes){
+    for (let i=0; i<bookingTimes.length; i++){
+        const button= document.createElement("button")
+        const input= document.createElement("input")
+        button.innerText = bookingTimes[i];
+        button.onclick = function bookTime(){
+            if (button.style.backgroundColor=="red"){
+                button.style.backgroundColor="green"
+            }
+            else {
+                button.style.backgroundColor="red"
+            }
+        }
+        bakgårdList.appendChild(document.createElement("br"))
+        bakgårdList.appendChild(button)
+        bakgårdList.appendChild(input)
+        
+    }
 }
 
 function slideShow(){
@@ -152,8 +111,23 @@ if(document.body.id == "index"){
     var submitted = false
     document.addEventListener("submit", sendEpost)
 } else if (document.body.id == "booking"){
-    booking() // denne her bør vi kanskje løse opp og rydde i så vi ikke definerer funksjoner i funksjonen booking
-    // gjorde dette bare midlertidig for å få ting på plass
-} else if (document.body.id == "galleri"){
+    const bookingTimes = ["06:00","07:00", "08:00", "09:00",
+             "10:00", "11:00", "12:00", "13:00", "14:00",
+             "15:00", "16:00", "17:00", "18:00", "19:00", 
+             "20:00", "21:00", "22:00", "23:00"]
 
+    const vaskeriList = document.getElementById("vaskeriList")
+    vaskeriList.innerText = "Vaskeri"
+
+    const bakgårdList = document.getElementById("bakgårdList")
+    bakgårdList.innerText = "Bakgård"
+
+    makeButtonsforVaskeri(bookingTimes);
+    makeButtonsforBakgård(bookingTimes);
+
+    // lage en array med klokketidet
+    // append child button og button text og id
+    //lage elementer dånn x
+} else if (document.body.id == "galleri"){
+    console.log("galleri")
 }
